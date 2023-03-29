cd04155638658d86bf3dddfedc27db070475b145
Maxim-87 <maksboks87@mail.ru>
2023-03-29 10:18:25 +0600

    refactor line-chart

--- src/app/01_line_chart/line-chart.component.ts
+++ src/app/01_line_chart/line-chart.component.ts
@@ -34,8 +34,8 @@ export class LineChartComponent implements OnInit {
     ngOnInit() {
         this.initSvg();
         this.initAxis();
-        this.drawAxis();
-        this.drawLine();
+        this.drawAxis(STOCKS);
+        this.drawLine(STOCKS);
     }
 
     private initSvg() {
@@ -45,16 +45,17 @@ export class LineChartComponent implements OnInit {
     }
 
     private initAxis() {
-        this.x = d3Scale.scaleTime().range([0, this.width]);
-        this.y = d3Scale.scaleLinear().range([this.height, 0]);
-        this.x.domain(d3Array.extent(STOCKS, (d) => d.date ));
-        this.y.domain(d3Array.extent(STOCKS, (d) => d.value ));
+        this.x = d3Scale.scaleTime().range([0, this.width]); // create timer scale
+        this.y = d3Scale.scaleLinear().range([this.height, 0]); // create numbers scale
+        this.x.domain(d3Array.extent(STOCKS, (d) => d.date )); // add data in scale
+        this.y.domain(d3Array.extent(STOCKS, (d) => d.value )); // add data in scale
     }
 
-    private drawAxis() {
+    private drawAxis(data: any) {
 
         this.svg.append('g')
             .attr('class', 'axis axis--x')
+
             .attr('transform', 'translate(0,' + this.height + ')')
             .call(d3Axis.axisBottom(this.x));
 
@@ -68,17 +69,51 @@ export class LineChartComponent implements OnInit {
             .attr('dy', '.71em')
             .style('text-anchor', 'end')
             .text('Price ($)');
+
+        const tooltipGroup = this.svg.append('g').attr('class', 'tooltip');
+
+        tooltipGroup
+            .selectAll('.tooltip-item')
+            .data(data)
+            .enter()
+            .append('g')
+            .attr('id', (d, i) => `tooltip-${i}`);
     }
 
-    private drawLine() {
+    private drawLine(data: any) {
         this.line = d3Shape.line()
             .x( (d: any) => this.x(d.date) )
             .y( (d: any) => this.y(d.value) );
 
         this.svg.append('path')
             .datum(STOCKS)
-            .attr('class', 'line')
-            .attr('d', this.line);
-    }
+            .attr('d', this.line)
+            .attr('aria-describedby', (d, i) => `tooltip-${i}`)
+            .on('mousemove', (d, i) => {
+                const { clientX, clientY } = d3.event;
+                d3.select(`#tooltip-${i}`)
+                    .attr('transform', `translate(${clientX} ${clientY})`);
+            })
+            .on('mouseenter', (d, i) => {
+                d3.select(`#tooltip-${i}`)
+                    .append('text')
+                    .text(`${d.value}`);
+            })
+            .on('focus', function(d, i) {
+                d3.select(this)
+                    .attr('stroke', 'black')
+                    .attr('stroke-width', 2);
+
+                const { top, right, bottom, left } = d3.event
+                    .target.getBoundingClientRect();
+                d3.select(`#tooltip-${i}`)
+                    .append('text')
+                    .text(`${d.value}`)
+                    .attr('transform',
+                        `translate(${(left + right) / 2} ${(top + bottom) / 2})`
+                    );
+            })
+            .attr('class', 'line');
+}
 
 }
